cb4f908cd29e40fc0f86efa2348357af308fa7c6
Asif Imran <asif.imran@adroll.com>
2021-04-16 16:58:14 +0000

    bump

--- .github/scripts/build_wheels.sh
+++ .github/scripts/build_wheels.sh
@@ -5,11 +5,5 @@ set -e
 set -x
 
 
-
-sudo apt-get install -y \
-  cmake \
-  libgflags-dev libsnappy-dev liblz4-dev libzstd-dev
-
-
 python -m pip install cibuildwheel
 python -m cibuildwheel --output-dir wheelhouse
